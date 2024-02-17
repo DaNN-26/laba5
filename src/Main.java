c56a17428d7c5458aa80777a5f6eddb736990dad
venom_play <idaniil535@gmail.com>
2024-02-17 10:57:49 +0300

    Upd

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Введите число x: ");
         int x = input.nextInt();
         Math_Expression exp1 = new Math_Expression(0, 0, x);
-        int result = exp1.FirstExpression(x);
+        int result = exp1.FirstExpression();
         System.out.printf("y = 3 * %d + 5 = %d", x, result);
         System.out.println();
 
@@ -14,19 +14,19 @@ public class Main {
         System.out.println("Введите число b: ");
         int b = input.nextInt();
         Math_Expression exp2 = new Math_Expression(a, b, 0);
-        result = exp2.SecondExpression(a, b);
+        result = exp2.SecondExpression();
         System.out.printf("y = (%d + %d) / (%d - %d) = %d", a, b, a, b, result);
         System.out.println();
 
         System.out.println("Введите число a: ");
         a = input.nextInt();
-        System.out.println("Введите число b: ");
-        b = input.nextInt();
         System.out.println("Введите число x: ");
         x = input.nextInt();
+        System.out.println("Введите число b: ");
+        b = input.nextInt();
         Math_Expression exp3 = new Math_Expression(a, b, x);
-        result = exp3.ThirstExpression(x, a, b);
-        System.out.printf("y = (%d * %d / %d )! = %d", a, x, b, result);
+        long long_result = exp3.ThirstExpression();
+        System.out.printf("y = (%d * %d / %d )! = %d", a, x, b, long_result);
         System.out.println();
     }
 }
@@ -41,20 +41,20 @@ class Math_Expression
         this.b = b;
         this.x = x;
     }
-    public int FirstExpression(int x)
+    public int FirstExpression()
     {
         int y = 3 * x + 5;
         return y;
     }
-    public int SecondExpression(int a, int b)
+    public int SecondExpression()
     {
         int y = (a + b) / (a - b);
         return y;
     }
-    public int ThirstExpression(int x, int a, int b)
+    public long ThirstExpression()
     {
         int y = a * x / b;
-        int n = 1;
+        long n = 1;
         for (int i = 1; i <= y; i++)
         {
             n = n * i;
